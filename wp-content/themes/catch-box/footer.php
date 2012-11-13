221ad47ff472c5c6fec246e688d4f85eb6aaaa15
Geronimo <gutierrez.geronimo@gmail.com>
2012-11-13 12:05:52 -0300

    removed 'powered by...'

--- wp-content/themes/catch-box/footer.php
+++ wp-content/themes/catch-box/footer.php
@@ -34,11 +34,11 @@
             		</a>
                     <?php esc_attr_e('. All Rights Reserved.', 'catchbox'); ?>
                 </div>
-                <div class="powered">
+                <!--<div class="powered">
                 	<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'catchbox' ) ); ?>" title="<?php esc_attr_e( 'Powered by WordPress', 'catchbox' ); ?>" rel="generator"><?php printf( __( 'Powered by %s', 'catchbox' ), 'WordPress' ); ?></a>
 					<span class="sep"> | </span>
                     <a href="<?php echo esc_url( __( 'http://catchthemes.com/', 'catchbox' ) ); ?>" title="<?php esc_attr_e( 'Theme Catch Box by Catch Internet', 'catchbox' ); ?>" rel="designer"><?php printf( __( 'Theme: %s', 'catchbox' ), 'Catch Box' ); ?></a>
-            	</div>
+            	</div>-->
                 <?php do_action( 'catchbox_startgenerator_close' ); ?>
           	</div> <!-- #site-generator -->
 	</footer><!-- #colophon -->
